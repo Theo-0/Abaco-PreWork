cbc12ad255790d0e6564aaf6233ac69b88f65789
Theo Ajuyah <44715145+Theo-0@users.noreply.github.com>
2020-08-03 07:22:26 +0100

    Update helloJenkinsC.c
    Removed additional part with assert; that's in sample_test

--- Jenkins/helloJenkinsC.c
+++ Jenkins/helloJenkinsC.c
@@ -5,14 +5,3 @@ int main() {
    printf("Hello World, Jenkins!");
    return 0;
 }
-
-// The below program runs fine because NDEBUG is defined 
-# define NDEBUG 
-# include <assert.h> 
-  
-int main() 
-{ 
-    int x = 7; 
-    assert (x==5); 
-    return 0; 
-} 
